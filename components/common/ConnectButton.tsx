4ce1834ec8d00d99ba65675eacb32e1f9e91a229
rhammy <olalekanrahman06@gmail.com>
2024-11-19 12:55:10 +0100

    modify connect button

--- components/common/ConnectButton.tsx
+++ components/common/ConnectButton.tsx
@@ -1,16 +1,29 @@
 import { client } from "@/app/client";
 import { ConnectButton as ConnectionButton } from "thirdweb/react";
+import { createWallet } from "thirdweb/wallets";
 
 import React from "react";
 
 export const ConnectButton = (props: IConnectButton) => {
+  const wallets = [
+    createWallet("io.metamask"),
+    createWallet("com.coinbase.wallet"),
+  ];
+
   return (
     <ConnectionButton
       client={client}
+      wallets={wallets}
       connectButton={{
         className:
           "!px-8 !py-3 !border-[3px] !text-base !border-purple-1 !bg-gradient-to-r !from-purple-main !via-purple-400 !to-purple-main !rounded-lg !font-bold !hover:shadow-lg !hover:shadow-purple-500/25 !transition-all !duration-300 !text-white",
         label: props.label ?? "Connect Wallet",
+      }}
+      connectModal={{
+        title: "Connect Wallet",
+        titleIcon: "/svg/logo.svg",
+        showThirdwebBranding: false,
+        size: "compact",
       }}
     />
   );
