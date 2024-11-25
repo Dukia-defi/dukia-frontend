6c5d8dabcfe68617a60fc286351ff54bf7b79aaf
obah <olusojiobah@gmail.com>
2024-11-25 18:24:21 +0100

    fix: resolved build errors

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -32,19 +32,6 @@ export const Hero = () => {
     },
   };
 
-  const logoRotate = {
-    initial: { rotate: -180, opacity: 0 },
-    animate: {
-      rotate: 0,
-      opacity: 1,
-      transition: {
-        type: "spring",
-        stiffness: 200,
-        damping: 20,
-      },
-    },
-  };
-
   return (
     <motion.section
       initial="initial"
