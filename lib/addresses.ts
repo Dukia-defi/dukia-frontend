212a90be9e73674b8d18edb3cdcb668bd99ef4e2
rhammy <olalekanrahman06@gmail.com>
2024-11-28 09:34:30 +0100

    update addresses

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -1,7 +1,7 @@
 export const deployed_contracts = {
   sepolia: {
     ccm: "0x2f43668479e1F2B5955f9C9bf1A93123a6738AF0",
-    aave: "0x412F05Ac9D672f883928906886C1a7b109004554",
+    aave: "0x560B163A0FBcC293E082Df70Cc6b26B655C99d36",
     batchExecutor: "0x111196aDC3868c745339F5D5549aB60D56B6c9F0",
     uniswap: "0x6b7683c33C142750AA36461F0f199d46dee28E9e",
   },
@@ -17,7 +17,7 @@ export const token_addresses = {
     usdc: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
     usdt: "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0",
     dai: "0xFF34B3d4Aee8ddCd6F9AFFFB6Fe49bD371b8a357",
-    link: "0x779877A7B0D9E8603169DdbD7836e478b4624789",
+    link: "0xf8Fb3713D459D7C1018BD0A49D19b4C44290EBE5",
   },
   lisk: {
     usdc: "",
